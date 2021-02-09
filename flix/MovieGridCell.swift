20d2ca868dd42041d40ee7955d1365509ed7f4b2
womfeld <womfeld@hawk.iit.edu>
2021-02-08 20:39:38 -0600

    Assignment 2 draft 1

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by School on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -85,6 +85,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //Get the new view controller using segue.destination
+        //Pass the selected object to the new view controller
+        
+        
+        
+        //Find the selected movie
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        
+        
+        //Pass the selected movie to the details controller
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie=movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 
 
 }
