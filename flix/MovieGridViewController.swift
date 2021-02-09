974ace695e8a84b3b662588d8dd1b4068cd6b001
womfeld <womfeld@hawk.iit.edu>
2021-02-08 20:56:34 -0600

    Assignment 2 complete, fixed collection view issue

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -31,6 +31,8 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         
+
+        
         
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
